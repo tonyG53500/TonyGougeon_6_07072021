ffbb8b8aaf5b43ae3d81aba6a2988be6d1206c90
Tony Gougeon <tony_gougeon@orange.fr>
2021-07-21 11:47:04 +0200

    add Database and Mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('requête recue!');
-    next();
-});
+mongoose.connect('mongodb+srv://tonyG:tonyg53500@cluster0.wvngz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
-    res.status(201);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+  });
 
-app.use((req, res, next) => {
-    res.json({ message: 'requete bien reçue !!!'});
-    next();
-});
+app.use(bodyParser.json());
 
-app.use((req, res) => {
-    console.log('Réponse recue avec succès !');
+app.post('/api/auth/signup', (req, res) => {
+    res.status(201).json({ message: "Compte créé !"});
 });
 
 module.exports = app;
